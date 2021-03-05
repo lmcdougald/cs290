8c189f6dd29e488a68b220343cff1cfffbd631c8
Luke McDougald <luke.mcdougald@outlook.com>
2021-03-05 12:41:38 -0800

    Added project start

---
+++ project/js/site.js
@@ -0,0 +1,30 @@
+﻿function toggleMenu()
+{
+    var isVisible = !$('.sidebar-head').hasClass('sidemenu-in');
+    $("#menu").attr("src", isVisible ? "/iconw/menu.svg" : "/iconw/x.svg");
+    $(".sidebar-head")
+        .toggleClass('sidemenu-in')
+        .animate(isVisible ? 'slidein' : 'slideout', 500, "linear");
+    $(".sidebar-rowbutton")
+        .toggleClass('sidemenu-in')
+        .animate(isVisible ? 'slidein' : 'slideout', 500, "linear");
+    $(".sidebar-rowhover")
+        .toggleClass('rowhover-in')
+        .animate(isVisible ? 'rowhoverin' : 'rowhoverout', 500, "linear");
+    if (isVisible) {
+        $("#body").unbind("click", toggleMenu);
+    }
+    else
+    {
+        $("#body").click(toggleMenu);
+    }
+}
+
+$("#menu").click(toggleMenu);
+
+$(".sidebar-rowhover").first().click(function (e) {
+    //views = viewManager();
+});
+$(".sidebar-rowhover").eq(2).click(function (e) {
+    //views = viewManager(1);
+});
