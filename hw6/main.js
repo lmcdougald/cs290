23c804b0b47999dbdc6342ec6ce81566061e7e03
Luke McDougald <luke.mcdougald@outlook.com>
2021-03-16 12:31:52 -0700

    Added static site, main.js not done though nor site.js

--- hw6/main.js
+++ hw6/main.js
@@ -7,6 +7,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 var hbs = exphbs.create({ defaultLayout: 'main.hbs'});
 var args = process.argv;
 
@@ -14,7 +16,6 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
 var pool = mysql.createPool({
-//    host  : '172.17.0.1',
     host  : args[2],
     user  : args[3],
     password: args[4],
@@ -25,21 +26,21 @@ app.set('port', 2880);
 
 app.get('/reset-table', function(req, res, next)
 {
-  var context = {};
-  pool.query("DROP TABLE IF EXISTS workouts", function(err){
-    var createString = "CREATE TABLE workouts("+
-    "id INT PRIMARY KEY AUTO_INCREMENT,"+
-    "name VARCHAR(255) NOT NULL,"+
-    "reps INT,"+
-    "weight INT,"+
-    "date DATE,"+
-    "lbs BOOLEAN)";
-    pool.query(createString, function(err)
-    {
-      context.results = "Table reset";
-      res.render('home', context);
-    })
-  });
+    var context = {};
+    pool.query("DROP TABLE IF EXISTS workouts", function(err){
+        var createString = "CREATE TABLE workouts("+
+        "id INT PRIMARY KEY AUTO_INCREMENT,"+
+        "name VARCHAR(255) NOT NULL,"+
+        "reps INT,"+
+        "weight INT,"+
+        "date DATE,"+
+        "lbs BOOLEAN)";
+        pool.query(createString, function(err)
+        {
+            context.results = "Table reset";
+            res.render('home', context);
+        })
+    });
 });
 
 
@@ -58,7 +59,46 @@ app.get('/insert',function(req, res, next){
 });
 
 
-app.get('/', function(req, res, next)
+app.get('/simple-update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
+    [req.query.name, req.query.done, req.query.due, req.query.id],
+    function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Updated " + result.changedRows + " rows.";
+    res.render('home',context);
+  });
+});
+
+
+app.get('/safe-update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("SELECT * FROM todo WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      var curVals = result[0];
+      mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
+        [req.query.name || curVals.name, req.query.done || curVals.done, req.query.due || curVals.due, req.query.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        context.results = "Updated " + result.changedRows + " rows.";
+        res.render('home',context);
+      });
+    }
+  });
+});
+
+
+app.get('/show', function(req, res, next)
 {
     var context = {};
     pool.query('SELECT * FROM workouts', function(err, rows, fields)
@@ -71,6 +111,7 @@ app.get('/', function(req, res, next)
         res.render('home', context);
     });
 });
+
 
 
 app.use(function(req,res){
